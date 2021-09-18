c4abbf8d25e6a2ec9d60d0c248aa34f0e097e9ed
CRECO <creaticoding@gmail.com>
2021-09-18 23:11:14 +0900

    Update key-creco-frontend.sh

--- key-creco-frontend.sh
+++ key-creco-frontend.sh
@@ -2,7 +2,7 @@ git clone https://github.com/CreatiCoding/creco-key
 cd creco-key
 cp creco-frontend.env ../.env
 cd ..
-rm -rf creco-script
+rm -rf creco-key
 
 cat .env | xargs -I {} echo "export {}" >>.envrc
 
